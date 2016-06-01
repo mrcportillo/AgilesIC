3351df81afb6c2a4c7fb373d653fd7ef904f09a9
root <root@mrc.mrc.dom>
2016-05-31 21:20:31 -0300

    Agregados metodos a clase main y clase de test unitarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,13 @@
  * Created by mrc on 31/05/16.
  */
 public class Main {
+
+    public static String holaMethod(String name) {
+        if (name.equalsIgnoreCase("marcos")) {
+            return "Hola señor " + name;
+        } else {
+            return "Hola " + name;
+        }
+    }
+
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+/**
+ * Created by mrc on 31/05/16.
+ */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    private final String HELO_METHOD_TEST_STRING = "Hola juan";
+    private final String HELO_METHOD_TEST_MARCOS_STRING = "Hola señor Marcos";
+    
+    @Test
+    public void heloMethodTest() {
+        String methodResult = Main.holaMethod("juan");
+        assertEquals("no iguales", methodResult, HELO_METHOD_TEST_STRING);
+    }
+
+    @Test
+    public void heloMethodMarcosTest() {
+        String methodResult = Main.holaMethod("Marcos");
+        assertEquals("no iguales", methodResult, HELO_METHOD_TEST_MARCOS_STRING);
+    }
+}
